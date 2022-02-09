2d06944471f913382fe5552cb0038fd9d5616702
Omid Shahsavarzadeh <omidsha@protonmail.com>
2022-02-09 17:54:55 +0330

    update version

--- defillama/__version__.py
+++ defillama/__version__.py
@@ -2,7 +2,7 @@
 __title__ = 'DeFiLlama'
 __description__ = 'Unofficial DeFi Llama API client.'
 __url__ = 'https://github.com/itzmestar/DeFiLlama'
-__version__ = '1.1.1'
+__version__ = '1.1.3'
 __build__ = 0x010001
 __author__ = 'Omid Sha'
 __author_email__ = 'omidsha@protonmail.com'
